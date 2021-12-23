924191a31813f46d12dd3110995684a4a40c28c4
Eduardo Picazzo <luis.arc.picazzo@me.com>
2021-12-23 02:14:40 -0600

    started ch 7 drills

---
+++ Working/Ex-1-Ch-7.cpp
@@ -0,0 +1,21 @@
+
+ 
+/*
+    1. Starting from the file calculator08buggy.cpp, get the calculator to compile.
+    2. Go through the entire progaram and add appropriate comments.
+    3. As you commented, you found errors (deviously inserted especially for you to find). Fix them; they are not in the text of the book.
+    4. Testing: preapare a set of inputs and use them to test the calcultator. Is your list pretty complete? What should you look for? Include negative values, 0 very small, very large, and "silly" inputs.
+    5. Do the testing and fix any bugs taht you missed when you commented.
+    6. Add a predefinede name k meaning 1000.
+    7. Give the usera a square root function sqrt(), for example, sqrt(2+6.7). Naturally, the value of sqrt(x) is the square root of x; for example, sqrt(9) is 3. Use the standard library sqrt() function
+       that is available through the header std_lib_facilities.h. Remember to update the comments, including the grammar.
+    8. Catch attempts to take the sqare root of a negative number an print an appropriate error mesage.
+    9. Allow the user to user pow(x, i) to mean "Multiply x with itself i times"; for example, pow(2.5, 3) is 2.5*2.5*2.5. Require i to be an integer using the technique we used for %.
+ */
+
+
+#include "std_lib_facilities.h"
+
+int main() {
+    
+}
