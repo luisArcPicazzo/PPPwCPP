951d4ba953fbf3417e730176b14c30f41f029c11
Eduardo Picazzo <luis.arc.picazzo@me.com>
2022-01-01 01:11:15 -0600

    renamed files added ex 3 ch 7

--- Working/Ex-3-Ch-7.cpp
+++ Working/Ex-3-Ch-7.cpp
@@ -1,9 +1,12 @@
 
 
 /*
-    2. Provide an assignment operator, =, so that you can change the value of a
-       variable after you introduce it using "let". Discuss why that can be useful
-       and how it can be a source of problems.
+    3. Provide named constants that you really can't change the value of. Hint:
+       You have to add a member to Variable that distinguishes between constants
+       and variable and check for it in set_value(). If you want to let the user
+       define constants (rather than just having pi and e defined as constants),
+       you'll have to add a notation to let the user express that, for example,
+       const pi = 3.14;
  */
 
 
